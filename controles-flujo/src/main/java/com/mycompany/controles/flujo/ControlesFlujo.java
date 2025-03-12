fe63642575ea71834f9755d5b22048ad45c2f5bd
oscardcd <oscardcd17@gmail.com>
2025-03-12 16:14:56 -0500

    add new project to show flow control

---
+++ controles-flujo/src/main/java/com/mycompany/controles/flujo/ControlesFlujo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.controles.flujo;
+
+/**
+ *
+ * @author oscarcorrea
+ */
+public class ControlesFlujo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
